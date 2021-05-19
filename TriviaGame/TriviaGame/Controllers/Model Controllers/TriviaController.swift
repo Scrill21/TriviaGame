7a63e5b8a42cb4d5ab889597b3e23d6e78a9d93a
Anthony Byrd <scrill21@gmail.com>
2021-05-19 17:34:36 -0400

    Updated Trivia Model, Model Controller, and NetworkError

--- TriviaGame/TriviaGame/Controllers/Model Controllers/TriviaController.swift
+++ TriviaGame/TriviaGame/Controllers/Model Controllers/TriviaController.swift
@@ -4,7 +4,54 @@
 //
 //  Created by anthony byrd on 5/19/21.
 //
+
 import Foundation
 
-class Tri
-
+class TriviaController {
+    
+    //MARK: - String Constants
+    //https://opentdb.com/api.php
+    static let baseURL = URL(string: "https://opentdb.com/api.php")
+    static let categoryComponent = "category"
+    static let difficultyComponent = "difficulty"
+    static let questionComponent = "question"
+    static let correctComponent = "correct"
+    static let incorrectComponent = "incorrect"
+    static let amountKey = "amount"
+    static let amountKeyValue = "10"
+    static let typeKey = "type"
+    static let typeKeyValue = "multiple"
+    
+    //MARK: - Methods
+    static func fetchTriviaAmount(completion: @escaping (Result<[Results], NetworkError>) -> Void) {
+        guard let baseURL = baseURL else {return completion(.failure(.invalidURL))}
+        
+        //Creating components and queries
+        var component = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let amountQuery = URLQueryItem(name: amountKey, value: amountKeyValue)
+        let typeQuery = URLQueryItem(name: typeKey, value: typeKeyValue)
+        component?.queryItems = [amountQuery, typeQuery]
+        
+        guard let finalURL = component?.url else {return completion(.failure(.invalidURL))}
+        print(finalURL)
+        
+        URLSession.shared.dataTask(with: finalURL) { data, response, error in
+            if let error = error {
+                return completion(.failure(.thrownError(error)))
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("TRIVIA REPONSE CODE: \(response)")
+            }
+            
+            guard let data = data else {return completion(.failure(.noData))}
+            do {
+                let trivia = try JSONDecoder().decode(TopLevelObject.self, from: data)
+                let triviaArray = trivia.results
+                return completion(.success(triviaArray))
+            } catch {
+                completion(.failure(.thrownError(error)))
+            }
+        }.resume()
+    }
+}//End of class
